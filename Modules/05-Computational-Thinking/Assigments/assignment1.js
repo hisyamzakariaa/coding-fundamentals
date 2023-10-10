2d5a75e866c77b63758a152d7ae77cbd5a4e8c38
hisyamzakariaa <135838950+hisyamzakariaa@users.noreply.github.com>
2023-10-10 16:17:41 +0800

    Add files via upload

---
+++ Modules/05-Computational-Thinking/Assigments/assignment1.js
@@ -0,0 +1,13 @@
+function possibleBonus(a,b){
+    if (a < b){
+        if (b - a <= 6)
+        console.log(true)
+    else{
+        console.log(false)
+    }
+    }
+    else console.log(false)
+}
+
+possibleBonus(5,3)
+
